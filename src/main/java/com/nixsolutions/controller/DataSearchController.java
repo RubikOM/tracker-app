25bbc4b6239c4366cf581ae11d2e5aa3753c8f8f
Mikhail Rubinskyi <mikhail.rubinskyi@nixsolutions.com>
2019-03-14 18:18:00 +0200

    Small code cleaning

--- src/main/java/com/nixsolutions/controller/DataSearchController.java
+++ src/main/java/com/nixsolutions/controller/DataSearchController.java
@@ -10,17 +10,6 @@ import com.nixsolutions.entity.DictionaryElement;
 @RestController
 public class DataSearchController {
 
-    /*@GetMapping("/fillPage/{word}")
-    public RedirectView returnWordTranslationFromApi(@PathVariable String word, RedirectAttributes redir) {
-        DictionaryElement result = new DictionaryElement.Builder(word, "mock").build();
-        redir.addFlashAttribute("dictionaryElement", result);
-
-        RedirectView redirectView = new RedirectView();
-        redirectView.setContextRelative(true);
-        redirectView.setUrl("/dictionary");
-        return redirectView;
-    }*/
-
     @GetMapping("/fillPage/{word}")
     public DictionaryElement returnWordTranslationFromApi(@PathVariable String word) {
         DictionaryElement result = new DictionaryElement.Builder(word, "mock").build();
