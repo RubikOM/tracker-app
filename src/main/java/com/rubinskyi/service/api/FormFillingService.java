f1fe825af6f89cbc1bce66036beaedb7eed7bea8
Mikhail Rubinskyi <mikhail.rubinskyi@nixsolutions.com>
2019-04-26 16:17:31 +0300

    Another portion of refactoring interfaces

--- src/main/java/com/rubinskyi/service/api/FormFillingService.java
+++ src/main/java/com/rubinskyi/service/api/FormFillingService.java
@@ -1,47 +1,10 @@
 package com.rubinskyi.service.api;
 
 import java.security.Principal;
-import java.util.Map;
-
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Service;
-import org.springframework.transaction.annotation.Transactional;
 
 import com.rubinskyi.entity.DictionaryElement;
-import com.rubinskyi.entity.User;
-import com.rubinskyi.service.UserService;
 
-@Service
-@Transactional
-public class FormFillingService {
-    private final PartialDataService partialDataService;
-    private final ComprehensiveDataService comprehensiveDataService;
-    private final UserService userService;
+public interface FormFillingService {
 
-    public FormFillingService(@Autowired PartialDataService partialDataService, ComprehensiveDataService comprehensiveDataService,
-                              UserService userService) {
-        this.partialDataService = partialDataService;
-        this.comprehensiveDataService = comprehensiveDataService;
-        this.userService = userService;
-    }
-
-    public DictionaryElement getDictionaryElementFromApi(String wordInEnglish, Principal principal) {
-        User user = userService.findByLogin(principal.getName());
-        String translation = partialDataService.obtainTranslationFromApi(wordInEnglish);
-        Map additionalData = comprehensiveDataService.obtainDataFromApi(wordInEnglish, user);
-
-        String transcription = (String) additionalData.getOrDefault("transcription", "");
-        String example = (String) additionalData.getOrDefault("example", "");
-        String exampleTranslation = (String) additionalData.getOrDefault("exampleTranslation", "");
-
-        return new DictionaryElement.Builder(wordInEnglish, removeExtraSymbols(translation))
-                .transcription("[" + transcription + "]")
-                .example(removeExtraSymbols(example))
-                .exampleTranslation(removeExtraSymbols(exampleTranslation))
-                .build();
-    }
-
-    private String removeExtraSymbols(String stringToClean) {
-        return stringToClean.replace(";", "");
-    }
+    DictionaryElement getDictionaryElementFromApi(String wordInEnglish, Principal principal);
 }
--- src/main/java/com/rubinskyi/service/api/PartialDataService.java
+++ src/main/java/com/rubinskyi/service/api/PartialDataService.java
@@ -1,51 +1,7 @@
 package com.rubinskyi.service.api;
 
-import java.io.IOException;
+public interface PartialDataService {
 
-import org.jetbrains.annotations.NotNull;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.context.annotation.PropertySource;
-import org.springframework.http.ResponseEntity;
-import org.springframework.stereotype.Service;
-import org.springframework.web.client.RestTemplate;
-
-import com.fasterxml.jackson.databind.ObjectMapper;
-import com.rubinskyi.pojo.api.PartialElement;
-
-@Service
-@PropertySource("classpath:api.properties")
-public class PartialDataService {
-    @Value("${partialDataCall}")
-    private String API_CALL_TEMPLATE_PARTIAL;
-    private static final Logger LOGGER = LoggerFactory.getLogger(PartialDataService.class);
-
-    public String obtainTranslationFromApi(String wordInEnglish) {
-        RestTemplate restTemplate = new RestTemplate();
-        String apiCall = String.format(API_CALL_TEMPLATE_PARTIAL, makeWordValidToUrl(wordInEnglish));
-
-        ResponseEntity<String> responseEntity = restTemplate.getForEntity(apiCall, String.class);
-        PartialElement partialElement = mapJsonToMinicard(responseEntity);
-        return partialElement.getTranslation().getTranslations();
-    }
-
-    private PartialElement mapJsonToMinicard(@NotNull ResponseEntity<String> responseEntity) {
-        PartialElement element;
-        ObjectMapper mapper = new ObjectMapper();
-
-        String jsonInput = responseEntity.getBody();
-        try {
-            element = mapper.readValue(jsonInput, PartialElement.class);
-            return element;
-        } catch (IOException e) {
-            LOGGER.error(e.toString(), e);
-            return new PartialElement();
-        }
-    }
-
-    private String makeWordValidToUrl(@NotNull String word) {
-        String[] wordAsArray = word.split(" ");
-        return wordAsArray.length > 0 ? wordAsArray[wordAsArray.length - 1] : word;
-    }
+    // TODO extending here?????
+    public String obtainTranslationFromApi(String wordInEnglish);
 }
