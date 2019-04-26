8e4258d0c9807844941e1b646e67d9cf4d0e9890
Mikhail Rubinskyi <mikhail.rubinskyi@nixsolutions.com>
2019-04-26 16:08:44 +0300

    small changes

--- src/main/java/com/rubinskyi/service/UserService.java
+++ src/main/java/com/rubinskyi/service/UserService.java
@@ -1,22 +1,7 @@
 package com.rubinskyi.service;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Service;
-
-import com.rubinskyi.dao.UserDao;
 import com.rubinskyi.entity.User;
 
-@Service
-public class UserService {
-
-    private final UserDao userDao;
-
-    @Autowired
-    public UserService(UserDao userDao) {
-        this.userDao = userDao;
-    }
-
-    public User findByLogin(String login) {
-        return userDao.findByLogin(login);
-    }
+public interface UserService {
+    User findByLogin(String login);
 }
