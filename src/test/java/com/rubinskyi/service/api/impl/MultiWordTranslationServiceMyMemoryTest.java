2815820586a63bcfd0e6cf054c7eab42bd1644b4
RubikOm <mykhailo.rubinskyi@gmail.com>
2019-08-08 17:36:37 +0300

    Improved string sending logics

--- src/test/java/com/rubinskyi/service/api/impl/MultiWordTranslationServiceMyMemoryTest.java
+++ src/test/java/com/rubinskyi/service/api/impl/MultiWordTranslationServiceMyMemoryTest.java
@@ -1,6 +1,7 @@
 package com.rubinskyi.service.api.impl;
 
 import com.rubinskyi.config.SpringTestConfig;
+import org.junit.Ignore;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -22,5 +23,27 @@ public class MultiWordTranslationServiceMyMemoryTest {
 
         String expectedResult = "ПРЕДПОСЫЛКИ Для того, чтобы максимально использовать это, вам нужно иметь немного опыта программирования.";
         assertEquals(expectedResult, multiWordTranslation.translateSentenceToRussian(uncroppedString));
+    }
+
+    // API has amount of words a day to translate that's why @Ignore takes a place here
+    @Test
+    @Ignore
+    public void translateSentenceToRussian_TranslatesUpTo500Characters() {
+        String initialString = "PREREQUISITES\n" +
+                "\n" +
+                "In order to make the most of this, you will need to have\n" +
+                "a little bit of programming experience. All examples in this\n" +
+                "book are in the Python programming language. Familiarity\n" +
+                "with Python or other scripting languages is suggested, but\n" +
+                "not required.\n" +
+                "\n" +
+                "You'll also need to know some basic mathematics. This\n" +
+                "book is hands-on and example driven: lots of examples and\n" +
+                "lots of code, so even if your math skills are not up to par,\n" +
+                "do not worry! The examples are very detailed and heavily\n" +
+                "documented to help you follow along.\nAnd one more question? To test.";
+
+        String expectedResult = "ПРЕДПОСЫЛКИ Для того, чтобы максимально использовать это, вам нужно иметь немного опыта программирования. Все примеры в этой книге на языке программирования Python. Знакомство с Python или другими языками сценариев предлагается, но не обязательно. Вам также нужно знать основы математики. Эта книга написана на практике и основана на примерах: много примеров и много кода, поэтому, даже если ваши математические навыки не на высоте, не беспокойтесь!";
+        assertEquals(expectedResult, multiWordTranslation.translateSentenceToRussian(initialString));
     }
 }
