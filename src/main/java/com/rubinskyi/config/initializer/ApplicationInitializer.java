7032c827f87043fa50d458b41515b06693ba592a
RubikOm <mykhailo.rubinskyi@gmail.com>
2019-08-12 16:28:52 +0300

    Changed Tomcats WebApplicationInitializer implementation to declare packages via ComponentScan

--- src/main/java/com/rubinskyi/config/initializer/ApplicationInitializer.java
+++ src/main/java/com/rubinskyi/config/initializer/ApplicationInitializer.java
@@ -1,44 +1,33 @@
 package com.rubinskyi.config.initializer;
 
-import java.util.EnumSet;
+import org.springframework.core.Ordered;
+import org.springframework.core.annotation.Order;
+import org.springframework.web.WebApplicationInitializer;
+import org.springframework.web.context.ContextLoaderListener;
+import org.springframework.web.context.support.AnnotationConfigWebApplicationContext;
+import org.springframework.web.filter.CharacterEncodingFilter;
+import org.springframework.web.servlet.DispatcherServlet;
 
 import javax.servlet.DispatcherType;
 import javax.servlet.FilterRegistration;
 import javax.servlet.ServletContext;
 import javax.servlet.ServletRegistration;
-
-import com.rubinskyi.config.bean.ObjectMapperBeans;
-import com.rubinskyi.config.bean.ThreadPoolBeans;
-import org.springframework.core.Ordered;
-import org.springframework.core.annotation.Order;
-import org.springframework.web.WebApplicationInitializer;
-import org.springframework.web.context.support.AnnotationConfigWebApplicationContext;
-import org.springframework.web.filter.CharacterEncodingFilter;
-import org.springframework.web.servlet.DispatcherServlet;
-
-import com.rubinskyi.config.JpaConfiguration;
-import com.rubinskyi.config.SpringConfiguration;
-import com.rubinskyi.config.WebAppConfig;
-import com.rubinskyi.config.security.SecurityConfig;
+import java.util.EnumSet;
 
 @Order(Ordered.HIGHEST_PRECEDENCE)
 public class ApplicationInitializer implements WebApplicationInitializer {
     private static final String DISPATCHER_SERVLET_NAME = "dispatcher";
     private static final String DISPATCHER_SERVLET_MAPPING = "/";
+    private static final String CONFIGURATION_LOCATION_PACKAGE = "com.rubinskyi.config";
 
     @Override
     public void onStartup(ServletContext container) {
-        AnnotationConfigWebApplicationContext ctx = new AnnotationConfigWebApplicationContext();
-        ctx.register(SpringConfiguration.class);
-        ctx.register(SecurityConfig.class);
-        ctx.register(WebAppConfig.class);
-        ctx.register(JpaConfiguration.class);
-        ctx.register(ObjectMapperBeans.class);
-        ctx.register(ThreadPoolBeans.class);
-        ctx.setServletContext(container);
+        AnnotationConfigWebApplicationContext context = new AnnotationConfigWebApplicationContext();
+        context.setConfigLocation(CONFIGURATION_LOCATION_PACKAGE);
+        container.addListener(new ContextLoaderListener(context));
 
         ServletRegistration.Dynamic dispatcherServlet = container.addServlet(
-                DISPATCHER_SERVLET_NAME, new DispatcherServlet(ctx));
+                DISPATCHER_SERVLET_NAME, new DispatcherServlet(context));
 
         dispatcherServlet.setLoadOnStartup(1);
         dispatcherServlet.addMapping(DISPATCHER_SERVLET_MAPPING);
